c4b1f5f2190aac9d3259ae1cc316c03d065686d9
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2019-04-26 00:25:15 -0400

    update gmp, install gmp dependencies in docker

--- script/ci_emscripten.sh
+++ script/ci_emscripten.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+apt-get update
+apt-get install -y m4
 mkdir build
 cd build
-emconfigure cmake ../src/ -DCMAKE_BUILD_TYPE=Emscripten
-emmake make # -j2 doesn't work here because libgmp has to be built
+emconfigure cmake ../src -DCMAKE_BUILD_TYPE=Emscripten
+NODE_OPTIONS="--max-old-space-size=4096" emmake make # -j2 doesn't work here because libgmp has to be built
 travis_wait 60 ctest -j2 --output-on-failure
 cd ..
