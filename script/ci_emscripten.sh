39db493866d45cf6c0078b4cf0a70333874baec4
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2019-04-28 02:42:41 -0400

    travis_wait doesn't play well with docker exec

--- script/ci_emscripten.sh
+++ script/ci_emscripten.sh
@@ -6,5 +6,5 @@ mkdir build
 cd build
 emconfigure cmake ../src -DCMAKE_BUILD_TYPE=Emscripten
 NODE_OPTIONS="--max-old-space-size=4096" emmake make # -j2 leads to intermittent build errors
-travis_wait 60 ctest -j2 --output-on-failure
+ctest -j2 --output-on-failure
 cd ..
