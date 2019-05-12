9e5902422746a0cb6458c426ae290495885f3ed0
Simon Hudon <simon.hudon@gmail.com>
2019-05-12 16:26:11 -0400

    add tracing for Travis

--- script/ci_emscripten_zip.sh
+++ script/ci_emscripten_zip.sh
@@ -2,4 +2,8 @@
 set -e
 # can't run build/shell/lean directly since the path in it is set by the docker container
 LEAN_VERSION_DIGITS=$(node build/shell/lean.js -v | sed 's/Lean (version \([^,]*\),.*/\1/')
+echo "tracing"
+echo "- " $LEAN_VERSION_DIGITS
+echo "- " lean-$LEAN_VERSION_DIGITS-$LEAN_VERSION_STRING-browser.zip
+ls build/
 zip build/lean-$LEAN_VERSION_DIGITS-$LEAN_VERSION_STRING-browser.zip build/shell/lean_js_*
