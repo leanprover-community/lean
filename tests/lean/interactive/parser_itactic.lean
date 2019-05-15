f02fc18def571609601c9d8660bc486d55494754
Keeley Hoek <keeley@hoek.io>
2019-05-16 08:10:43 +0930

    feat(interactive): add def for parsing raw `expr`s (#33)

--- tests/lean/interactive/parser_itactic.lean
+++ tests/lean/interactive/parser_itactic.lean
@@ -11,10 +11,14 @@ match p with
 | none := tactic.trace "nothing"
 end
 
+meta def my_tac_raw (e : interactive.parse (lean.parser.reflect $ optional lean.parser.itactic)) : tactic unit :=
+tactic.trace e
+
 end tactic.interactive
 
 meta def quote_tac : tactic unit := `[my_tac {tactic.trace "hi2"}]
 meta def quote_tac_optional : tactic unit := `[my_tac_optional]
+meta def quote_tac_raw : tactic unit := `[my_tac_raw {tactic.trace "hi2"}]
 
 example : true := begin
     my_tac {tactic.trace "hi"},
@@ -23,8 +27,13 @@ example : true := begin
     my_tac_optional {},
     my_tac_optional,
 
+    my_tac_raw {tactic.trace "hi"},
+    my_tac_raw {},
+    my_tac_raw,
+
     quote_tac,
     quote_tac_optional,
+    quote_tac_raw,
 
     trivial
 end
