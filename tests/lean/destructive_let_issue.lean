b747c076831e2fc19b6a53a4c765df4aa62b9a52
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2019-11-09 19:07:39 +0000

    fix(55): Add check to `mk_class_instance_at`. Fixes #55
    This is a bug because it is not necessarily true that
    two local contexts are equal when their local instances are equal.

--- tests/lean/destructive_let_issue.lean
+++ tests/lean/destructive_let_issue.lean
@@ -1,5 +1,9 @@
 /- Bug documented at https://github.com/leanprover-community/lean/issues/55  -/
 
+-- set_option trace.elaborator_detail true
+-- set_option trace.type_context.is_def_eq true
+-- set_option trace.type_context.is_def_eq_detail true
+
 example : bool :=
   let x : false → Prop := λ x, match x with end in
   let x : bool := match 4 with
@@ -13,5 +17,21 @@ example : bool :=
   end
 
 example : ℕ → bool
-| 0 := let _ := 3 in tt
+| 0 := tt
 | (k+1) := k = k
+
+example : ℕ → bool
+| 0 := let x := tt in tt
+| (k+1) := k = k
+
+example : ℕ → bool
+| 0 := let _ := tt in tt
+| (k+1) := k = k
+
+example : ℕ → bool
+| 0 := let _ := tt in tt
+| (k+1) := if k = k then tt else ff
+
+example : ℕ → bool
+| 0 := let _ := tt in tt
+| (k+1) := if h : k = k then tt else ff -- succeeds
