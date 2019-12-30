41c37f959cc66da35545880f21d732d3e5971524
Simon Hudon <simon.hudon@gmail.com>
2019-12-30 12:01:22 -0500

    ci script

---
+++ script/ci.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+mkdir build
+cd build
+# eval cmake ../src $CMAKE_OPTIONS
+eval cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+                -DCMAKE_CXX_COMPILER=$CXX_FAMILY \
+                -DTESTCOV=$TESTCOV \
+                -DTCMALLOC=$TCMALLOC \
+                -DMULTI_THREAD=$MULTI_THREAD \
+                -DSTATIC=$STATIC \
+                -DLEAN_EXTRA_MAKE_OPTS=$LEAN_EXTRA_MAKE_OPTS \
+                $OPTIONS \
+                ../src
+cmake --build . -j4
+cpack
