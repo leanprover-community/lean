0f51245d4841fe9674a08261ecdfcb7859956f11
Simon Hudon <simon.hudon@gmail.com>
2019-12-31 00:43:29 -0500

    windows flags

--- script/ci-win.sh
+++ script/ci-win.sh
@@ -6,12 +6,16 @@ cd build
 if [ $CFG == 'MINGW64' ]; then
     OPTIONS=''
     cmake ../src -DINCLUDE_MSYS2_DLLS=ON -DCMAKE_BUILD_TYPE=Release $OPTIONS -G 'Unix Makefiles'
+              -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 else
     cmake ../src -DCMAKE_BUILD_TYPE=Release
               -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake
+              -DLEAN_EXTRA_C_FLAGS=/GL-
               -DLEAN_EXTRA_CXX_FLAGS=/GL-
               -DLEAN_EXTRA_LINKER_FLAGS_MSVC=/LTCG:OFF
-              -G "NMake Makefiles"
+              # -G "NMake Makefiles"
+              -G "Unix Makefiles"
+              -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 fi
 
 cmake --build . -j4
