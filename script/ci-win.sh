2f31dfd20740b9e51c9a5569966e9393b1360139
Simon Hudon <simon.hudon@gmail.com>
2020-01-03 12:17:04 -0500

    backslash

--- script/ci-win.sh
+++ script/ci-win.sh
@@ -6,15 +6,15 @@ cd build
 
 if [ $CFG == 'MINGW64' ]; then
     OPTIONS=''
-    cmake ../src -DINCLUDE_MSYS2_DLLS=ON -DCMAKE_BUILD_TYPE=Release $OPTIONS -G 'Unix Makefiles'
+    cmake ../src -DINCLUDE_MSYS2_DLLS=ON -DCMAKE_BUILD_TYPE=Release $OPTIONS -G 'Unix Makefiles' \
               -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 else
-    cmake ../src -DCMAKE_BUILD_TYPE=Release
-              -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake
-              -DLEAN_EXTRA_C_FLAGS=/GL-
-              -DLEAN_EXTRA_CXX_FLAGS=/GL-
-              -DLEAN_EXTRA_LINKER_FLAGS_MSVC=/LTCG:OFF
-              -G "Unix Makefiles"
+    cmake ../src -DCMAKE_BUILD_TYPE=Release \
+              -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake \
+              -DLEAN_EXTRA_C_FLAGS=/GL- \
+              -DLEAN_EXTRA_CXX_FLAGS=/GL- \
+              -DLEAN_EXTRA_LINKER_FLAGS_MSVC=/LTCG:OFF \
+              -G "Unix Makefiles" \
               -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
               # -G "NMake Makefiles"
 fi
