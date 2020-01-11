baa9c39d4e6083d3d2e734680658108cad611a8f
Simon Hudon <simon.hudon@gmail.com>
2020-01-11 17:04:39 -0500

    chore(*): add test for `change`

--- tests/lean/change_tac.lean
+++ tests/lean/change_tac.lean
@@ -45,3 +45,14 @@ example (h : 1 + 2 = 1 + 2 + 1) : 1 + 2 = 3 :=
 begin
   change 1 + 2 with 4 at *, -- ERROR
 end
+
+example : true :=
+begin
+  have : ∀ x : ℕ, x = x,
+  change ∀ y : ℕ, y = y,
+  trace_state,
+  admit,
+  change ∀ y : ℕ, y = y at this,
+  trace_state,
+  trivial
+end
