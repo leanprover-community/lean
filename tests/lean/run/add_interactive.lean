044878e6d04795759a1ad0c13f64f553edb46508
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-01-29 13:12:22 -0500

    reviewer comment

--- tests/lean/run/add_interactive.lean
+++ tests/lean/run/add_interactive.lean
@@ -8,6 +8,9 @@ tactic.interactive.exact q
 
 /- Copy tactic my_exact to tactic.interactive. -/
 run_cmd add_interactive [`my_exact]
+
+/- Copy tactic my_exact to test_namespace. -/
+run_cmd add_interactive [`my_exact] `test_namespace
 end bla
 
 example : true :=
@@ -20,6 +23,11 @@ open tactic
 run_cmd do
   old_doc ← doc_string `foo.bla.my_exact,
   new_doc ← doc_string `tactic.interactive.my_exact,
-  if old_doc = new_doc then skip else fail "doc strings do not match"
+  if old_doc = new_doc then skip else fail "doc strings of foo.bla.my_exact and tactic.interactive.my_exact do not match"
+
+run_cmd do
+  old_doc ← doc_string `foo.bla.my_exact,
+  new_doc ← doc_string `test_namespace.my_exact,
+  if old_doc = new_doc then skip else fail "doc strings of foo.bla.my_exact and test_namespace.my_exact do not match"
 
 end foo
