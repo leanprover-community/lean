bd43b291e75e9d6cd5ee7e53e477637f2bc8966a
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2020-03-06 20:47:26 +0000

    fix(test): deterministic tests

--- tests/lean/importing/test_importing.sh
+++ tests/lean/importing/test_importing.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 ulimit -s 8192
-LEAN=$1
+LEAN_MAKE="$1 -j0 --make"
 export LEAN_PATH=../../../library:.
 
 cat <<EOF > foo.lean
@@ -12,14 +12,14 @@ run_cmd tactic.trace "foo"
 EOF
 rm -f foo.olean
 
-out=$($LEAN --make ./foo.lean 2>&1)
+out=$($LEAN_MAKE ./foo.lean 2>&1)
 if [[ $out != "foo" ]]; then
     echo "-- Lean isn't building foo: got $out , expected foo"
     exit 1
 fi
 
 touch foo.lean
-out=$($LEAN --make ./foo.lean 2>&1)
+out=$($LEAN_MAKE ./foo.lean 2>&1)
 if [[ $out != "" ]]; then
     echo "-- Lean rebuilds foo on timestamp change: got $out , expected no output"
     exit 1
@@ -41,7 +41,7 @@ run_cmd tactic.trace "z"
 EOF
 rm -f x.olean y.olean z.olean
 
-out=$($LEAN --make z.lean 2>&1 | tr -d '\n')
+out=$($LEAN_MAKE z.lean 2>&1 | tr -d '\n')
 if [[ "$out" != "xyz" ]]; then
     echo "-- Wrong modules built: got $out , expected xyz"
     exit 1
@@ -52,7 +52,7 @@ def y := 1
 run_cmd tactic.trace "y"
 EOF
 
-out=$($LEAN --make z.lean 2>&1 | tr -d '\n')
+out=$($LEAN_MAKE z.lean 2>&1 | tr -d '\n')
 if [[ "$out" != "yz" ]]; then
     echo "-- Wrong modules built: got $out , expected yz"
     exit 1
@@ -64,14 +64,14 @@ def y := x
 run_cmd tactic.trace "y"
 EOF
 
-out=$($LEAN --make z.lean 2>&1 | tr -d '\n')
+out=$($LEAN_MAKE z.lean 2>&1 | tr -d '\n')
 if [[ "$out" != "yz" ]]; then
     echo "-- Wrong modules built: got $out , expected yz"
     exit 1
 fi
 
 touch x.lean
-out=$($LEAN --make z.lean 2>&1 | tr -d '\n')
+out=$($LEAN_MAKE z.lean 2>&1 | tr -d '\n')
 if [[ "$out" != "" ]]; then
     echo "-- Wrong modules built: got $out , expected no output"
     exit 1
