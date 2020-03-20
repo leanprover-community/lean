635f3ba172ecf7c192ee3b945b6e2d373ffcbcfc
Mario Carneiro <di.gama@gmail.com>
2020-03-20 01:43:47 -0700

    fix(frontends/lean/brackets): parse {a,b,c} as right associative

--- src/frontends/lean/brackets.cpp
+++ src/frontends/lean/brackets.cpp
@@ -45,16 +45,24 @@ static expr mk_singleton(parser & p, pos_info const & pos, expr const & e) {
 /* Parse rest of the insertable '{' expr ... */
 static expr parse_fin_set(parser & p, pos_info const & pos, expr const & e) {
     lean_assert(p.curr_is_token(get_comma_tk()) || p.curr_is_token(get_rcurly_tk()));
-    expr r = mk_singleton(p, pos, e);
+    buffer<pair<pos_info, expr>> stack;
+    stack.emplace_back(pos, e);
     while (p.curr_is_token(get_comma_tk())) {
         auto ins_pos = p.pos();
         p.next();
         if (p.curr_is_token(get_rcurly_tk())) break;
         expr e2 = p.parse_expr();
-        expr insert = p.save_pos(mk_constant(get_has_insert_insert_name()), ins_pos);
-        r = p.rec_save_pos(mk_app(insert, e2, r), ins_pos);
+        stack.emplace_back(ins_pos, e2);
     }
     p.check_token_next(get_rcurly_tk(), "invalid explicit finite collection, '}' expected");
+    unsigned i = stack.size() - 1;
+    auto [pos, e2] = stack[i];
+    expr r = mk_singleton(p, pos, e2);
+    while (i--) {
+        auto [pos, e2] = stack[i];
+        expr insert = p.save_pos(mk_constant(get_has_insert_insert_name()), pos);
+        r = p.rec_save_pos(mk_app(insert, e2, r), pos);
+    }
     return r;
 }
 
