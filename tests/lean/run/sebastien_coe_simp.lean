ef541bd3f93be88578ba2f71737af8c005023383
Gabriel Ebner <gebner@gebner.org>
2020-05-01 21:42:48 +0200

    feat(library/fun_info): improve congruence lemmas for coe_fn (#209)

---
+++ tests/lean/run/sebastien_coe_simp.lean
@@ -0,0 +1,20 @@
+variable (α : Type*)
+
+structure my_equiv :=
+(to_fun : α → α)
+
+instance : has_coe_to_fun (my_equiv α) := ⟨_, my_equiv.to_fun⟩
+
+def my_equiv1 : my_equiv α :=
+{ to_fun := id }
+
+def my_equiv2 : my_equiv α :=
+{ to_fun := id }
+
+@[simp] lemma one_eq_two : my_equiv1 α = my_equiv2 α := rfl
+
+lemma other (x : ℕ) : my_equiv1 ℕ (x + 0) = my_equiv2 ℕ x := by simp -- does not fail
+
+@[simp] lemma two_apply (x : α) : my_equiv2 α x = x := rfl
+
+lemma one_apply (x : α) : my_equiv1 α x = x := by simp -- does not fail
