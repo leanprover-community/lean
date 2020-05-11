7a1c469847e41f4f7db98da1df522935081a321e
Yury G. Kudryashov <urkud@urkud.name>
2020-05-11 08:25:58 +0000

    chore(library/init/function): use dot notation, add some docstrings (#216)
    Renamed (in `function` namespace):
    
    * `injective_comp`, `surjective_comp`, `bijective_comp`: `injective.comp`, `surjective.comp`, `bijective.comp`;
    * `injective_of_left_inverse` : `left_inverse.injective`;
    * `injective_of_has_left_inverse`: `has_left_inverse.injective`;
    * `surjective_of_has_right_inverse`: `has_right_inverse.surjective`;
    * `right_inverse.surjective (new);
    * `id_of_left_inverse`, `id_of_right_inverse`: `left_inverse.id`, `right_inverse.id` (both `protected`).

--- tests/lean/doc_strings.lean
+++ tests/lean/doc_strings.lean
@@ -14,5 +14,4 @@ open tactic
 run_cmd doc_string `foo.a >>= trace
 run_cmd doc_string `foo.b >>= trace
 run_cmd doc_string `two >>= trace
-run_cmd olean_doc_strings >>= trace
 run_cmd module_doc_strings >>= trace
