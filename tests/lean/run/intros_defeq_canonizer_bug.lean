9f4117601e9fc0f5da5c6ceded154aa0607014fe
Johan Commelin <johan@commelin.net>
2020-05-13 20:28:51 +0000

    Fix run test intros_defeq_canonizer_bug

--- tests/lean/run/intros_defeq_canonizer_bug.lean
+++ tests/lean/run/intros_defeq_canonizer_bug.lean
@@ -1,9 +1,12 @@
+class ring (α : Type*) extends has_zero α.
 constant {u} f {α : Type u} : α → α → α → α
 axiom {u} fax {α : Type u} [ring α] (a b : α) : f a b a = 0
 
 attribute [ematch] fax
 
 universe variables u
+
+class field (α : Type*) extends ring α, has_add α.
 
 lemma ex {α : Type u} [field α] (x y : α) : f (x + y) (y + x) (x + y) = 0 :=
 begin [smt] ematch end
