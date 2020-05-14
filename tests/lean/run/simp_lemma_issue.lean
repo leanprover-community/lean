4ff4eceeef7b17d7a7e6da4896a9730a41f561af
Johan Commelin <johan@commelin.net>
2020-05-14 09:24:07 +0000

    Fix run test simp_lemma_issue

--- tests/lean/run/simp_lemma_issue.lean
+++ tests/lean/run/simp_lemma_issue.lean
@@ -1,15 +1,23 @@
 universe variables u
 
+class comm_semiring (α : Type*) extends has_zero α, has_add α, has_one α, has_mul α.
+
+lemma zero_add {α : Type*} [comm_semiring α] (a : α) : (0:α) + a = a := sorry
+lemma add_zero {α : Type*} [comm_semiring α] (a : α) : a + 0 = a := sorry
+
+instance foo : comm_semiring nat :=
+{ zero := 0, one := 1, add := (+), mul := (*) }
+
 def ex {α : Type u} [comm_semiring α] (a : α) : 0 + a = a :=
 zero_add a
 
-local attribute [-simp] zero_add add_zero
+-- local attribute [-simp] zero_add add_zero
 attribute [simp] ex
 
 example (a b : nat) : 0 + 0 + a = a :=
 by simp
 
-local attribute [-ematch] zero_add add_zero
+-- local attribute [-ematch] zero_add add_zero
 attribute [ematch] ex
 
 example (a b : nat) : 0 + 0 + a = a :=
