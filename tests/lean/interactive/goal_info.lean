32687698901c44b12e9cb5dc2a17a4b8334af639
Johan Commelin <johan@commelin.net>
2020-05-14 04:36:57 +0000

    Hopefully fix interactive test goal_info

--- tests/lean/interactive/goal_info.lean
+++ tests/lean/interactive/goal_info.lean
@@ -33,7 +33,7 @@ begin
   intros n m,
   induction m with m' ih,
   {   change n + 0 = 0 + n, simp [zadd, nat.add_zero, nat.zero_add] },
-                                      --^ "command": "info"
+                                                                  --^ "command": "info"
   { change succ (n + m') = succ m' + n,
     rw [succ_add, ih]
   }
