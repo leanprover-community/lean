fdf0053274e30bb2b9ffffbc2ec1196750ad8c8f
Johan Commelin <johan@commelin.net>
2020-05-14 05:10:18 +0000

    Fix run test nat_sub_ematch

--- tests/lean/run/nat_sub_ematch.lean
+++ tests/lean/run/nat_sub_ematch.lean
@@ -1,6 +1,6 @@
 set_option trace.smt.ematch true
 
-example (a b c d : nat) (f : nat → nat) : f d = a + b → f d - a + c = c + b :=
+example (a b c d : nat) (f : nat → nat) : f d = a + b → f d - a + c = b + c :=
 begin [smt]
   intros,
   eblast
