7076b9ec36cb0544a4e9f0f7c9735b589bfaf378
Johan Commelin <johan@commelin.net>
2020-05-14 09:18:31 +0000

    Fix run test smt_ematch_alg_issue

--- tests/lean/run/smt_ematch_alg_issue.lean
+++ tests/lean/run/smt_ematch_alg_issue.lean
@@ -1,9 +1,15 @@
+class ring (α : Type*) extends has_zero α, has_add α, has_one α, has_mul α.
+
+lemma add_comm {α : Type*} [ring α] (a b : α) : a + b = b + a := sorry
+
 lemma {u} ring_add_comm {α : Type u} [ring α] : ∀ (a b : α), (: a + b :) = b + a :=
 add_comm
 
 open smt_tactic
 meta def no_ac : smt_config :=
 { cc_cfg := { ac := ff }}
+
+class field (α : Type*) extends ring α, has_inv α.
 
 lemma ex {α : Type} [field α] (a b : α) : a + b = b + a :=
 begin [smt] with no_ac,
