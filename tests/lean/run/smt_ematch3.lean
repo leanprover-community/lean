b4209e2379a27b9ed99faf0be291b7e701329cbe
Johan Commelin <johan@commelin.net>
2020-05-14 05:17:03 +0000

    Almost fix run test smt_ematch3

--- tests/lean/run/smt_ematch3.lean
+++ tests/lean/run/smt_ematch3.lean
@@ -1,13 +1,13 @@
 namespace Ex
 open nat
-notation `⟦`:max a `⟧`:0 := cast (by simp) a
+notation `⟦`:max a `⟧`:0 := cast (by simp [nat.add_zero]) a
 
 inductive vector (α : Type) : nat → Type
 | nil {} : vector 0
 | cons   : Π {n}, α → vector n → vector (succ n)
 
 namespace vector
-local attribute [simp] add_succ succ_add add_comm
+local attribute [simp] add_succ succ_add nat.add_comm
 
 variable {α : Type}
 
@@ -16,7 +16,7 @@ def app : Π {n m : nat}, vector α n → vector α m → vector α (n + m)
 | (succ n) m (cons a v) w := ⟦ cons a (app v w) ⟧
 
 lemma app_nil_right {n : nat} (v : vector α n) : app v nil == v :=
-begin induction v, reflexivity, {[smt] ematch_using [app, add_comm, zero_add, add_zero] }, end
+begin induction v, reflexivity, {[smt] ematch_using [app, nat.add_comm, nat.zero_add, nat.add_zero] }, end
 
 def smt_cfg : smt_config :=
 { cc_cfg := {ac := ff}}
@@ -26,8 +26,8 @@ lemma app_assoc {n₁ n₂ n₃ : nat} (v₁ : vector α n₁) (v₂ : vector α
 begin
   intros,
   induction v₁,
-  {[smt] ematch_using [app, zero_add] },
-  {[smt] with smt_cfg, iterate { ematch_using [app, add_succ, succ_add, add_comm, add_assoc] }}
+  {[smt] ematch_using [app, nat.zero_add] },
+  {[smt] with smt_cfg, iterate { ematch_using [app, add_succ, succ_add, nat.add_comm, nat.add_assoc] }}
 end
 
 def rev : Π {n : nat}, vector α n → vector α n
@@ -39,8 +39,8 @@ lemma rev_app : ∀ {n₁ n₂ : nat} (v₁ : vector α n₁) (v₂ : vector α 
 begin
   intros,
   induction v₁,
-  {[smt] iterate {ematch_using [app, rev, zero_add, add_zero, add_comm, app_nil_right]}},
-  {[smt] iterate {ematch_using [app, rev, zero_add, add_zero, add_comm, app_assoc, add_one]} }
+  {[smt] iterate {ematch_using [app, rev, nat.zero_add, nat.add_zero, nat.add_comm, app_nil_right]}},
+  {[smt] iterate {ematch_using [app, rev, nat.zero_add, nat.add_zero, nat.add_comm, app_assoc, add_one]} }
 end
 
 end vector
