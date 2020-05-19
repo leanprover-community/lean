7c4326d3c8c9d7b7300278d891d617376f1508b8
Gabriel Ebner <gebner@gebner.org>
2020-05-19 18:19:25 +0200

    Backport test case from Lean 4.

--- tests/lean/run/coe_fn_mvar.lean
+++ tests/lean/run/coe_fn_mvar.lean
@@ -8,3 +8,34 @@ def frob {α β} (a : α) : hom β (α × β) := ⟨λ b, (a, b)⟩
 def foo : ℤ × ℤ := frob 1 2
 
 example : foo = (1, 2) := rfl
+
+-- backport elabissues/Reid1.lean from Lean 4
+
+structure constantFunction (α β : Type) :=
+(f : α → β)
+(h : ∀ a₁ a₂, f a₁ = f a₂)
+
+instance {α β : Type} : has_coe_to_fun (constantFunction α β) :=
+⟨_, constantFunction.f⟩
+
+def myFun {α : Type} : constantFunction α (option α) :=
+{ f := fun a, none,
+  h := fun a₁ a₂, rfl }
+
+def myFun' (α : Type) : constantFunction α (option α) :=
+{ f := fun a, none,
+  h := fun a₁ a₂, rfl }
+
+#check myFun 3
+#check @myFun nat 3            -- works
+
+#check myFun' _ 3
+#check myFun' nat 3            -- works
+
+/-
+The single, double, and serif uparrows don't really work in Lean 3.
+
+#check ⇑myFun 3
+#check ⇑(myFun' _) 3
+#check ⇑(myFun' Nat) 3
+-/
