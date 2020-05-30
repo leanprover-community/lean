e082902f93894a66e4d0f5fb8d612832456a0c17
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2020-05-30 01:32:29 +0100

    style(local_context): remove names from instances

--- library/init/meta/local_context.lean
+++ library/init/meta/local_context.lean
@@ -1,5 +1,6 @@
 prelude
 import init.meta.name init.meta.expr init.data.option.basic
+
 meta structure local_decl :=
 (unique_name : name)
 (pp_name : name)
@@ -12,8 +13,8 @@ meta def local_decl.to_expr : local_decl → expr
 | ⟨un, pn, y, _, bi, _⟩ := expr.local_const un pn bi y
 
 /-- A local context is a list of local constant declarations.
-    Each metavariable in a metavariable context holds a local_context
-    to declare which locals the metavariable is allowed to depend on. -/
+Each metavariable in a metavariable context holds a local_context
+to declare which locals the metavariable is allowed to depend on. -/
 meta constant local_context : Type
 
 namespace local_context
@@ -21,7 +22,8 @@ namespace local_context
 /-- The empty local context. -/
 meta constant empty : local_context
 
-/-- Add a new local constant to the lc. The new local has an unused unique_name. Fails when the type depends on local constants that are not present in the context.-/
+/-- Add a new local constant to the lc. The new local has an unused unique_name.
+Fails when the type depends on local constants that are not present in the context.-/
 meta constant mk_local (pretty_name : name) (type : expr) (bi : binder_info) : local_context → option (expr × local_context)
 
 meta constant get_local_decl : name → local_context → option local_decl
@@ -40,19 +42,19 @@ list.reverse ∘ fold (λ acc e, e :: acc) []
 
 meta def to_format : local_context → format := to_fmt ∘ to_list
 
-meta instance lc_has_to_format : has_to_format local_context := ⟨to_format⟩
+meta instance : has_to_format local_context := ⟨to_format⟩
 
-meta instance lc_has_le : has_le local_context := ⟨λ a b, local_context.is_subset a b⟩
+meta instance : has_le local_context := ⟨λ a b, local_context.is_subset a b⟩
 
-meta instance lc_dec_le : decidable_rel ((≤) : local_context → local_context → Prop) := infer_instance
+meta instance : decidable_rel ((≤) : local_context → local_context → Prop) := infer_instance
 
-meta instance lc_has_emptyc : has_emptyc local_context := ⟨empty⟩
+meta instance : has_emptyc local_context := ⟨empty⟩
 
-meta instance lc_inhabited : inhabited local_context := ⟨empty⟩
+meta instance : inhabited local_context := ⟨empty⟩
 
-meta instance mem_expr_lc  : has_mem expr local_context :=
+meta instance : has_mem expr local_context :=
 ⟨λ e lc, option.is_some $ get_local (expr.local_uniq_name e) lc⟩
 
-meta instance mem_expr_lc_dec {e : expr} {lc : local_context} : decidable (e ∈ lc) := infer_instance
+meta instance {e : expr} {lc : local_context} : decidable (e ∈ lc) := infer_instance
 
 end local_context
