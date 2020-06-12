489dbce88238f45e2dfde1b80d5a7d3833acb23c
Scott Morrison <scott.morrison@gmail.com>
2020-06-12 10:46:20 +1000

    fix

--- library/init/data/int/basic.lean
+++ library/init/data/int/basic.lean
@@ -536,7 +536,7 @@ protected lemma distrib_right (a b c : ℤ) : (a + b) * c = a * c + b * c :=
 begin rw [int.mul_comm, int.distrib_left], simp [int.mul_comm] end
 
 protected lemma zero_ne_one : (0 : int) ≠ 1 :=
-assume h : 0 = 1, succ_ne_zero _ (int.of_nat_inj h).symm
+assume h : 0 = 1, succ_ne_zero _ (int.of_nat.inj h).symm
 
 lemma of_nat_sub {n m : ℕ} (h : m ≤ n) : of_nat (n - m) = of_nat n - of_nat m :=
 show of_nat (n - m) = of_nat n + neg_of_nat m, from match m, h with
