392dc855f59727ea50ad184220eec1f36a6e2f78
Gabriel Ebner <gebner@gebner.org>
2020-06-16 11:16:25 +0200

    fix(frontends/lean/structure_cmd): remove as-is annotations
    Fixes #334.

--- src/frontends/lean/structure_cmd.cpp
+++ src/frontends/lean/structure_cmd.cpp
@@ -538,7 +538,7 @@ struct structure_cmd_fn {
                 buffer<name> lp_names;
                 buffer<expr> ctx;
                 collect_implicit_locals(m_p, lp_names, ctx, tmp);
-                return elaborate_for_each<expr>(ctx, tmp, std::bind(&structure_cmd_fn::elaborate_local, this, true, _1, _2, _3, _4), [&](expr tmp) {
+                return elaborate_for_each<expr>(ctx, tmp, std::bind(&structure_cmd_fn::elaborate_local, this, false, _1, _2, _3, _4), [&](expr tmp) {
                     level_param_names new_ls;
                     expr new_tmp;
                     std::tie(new_tmp, new_ls) = m_p.elaborate_type(m_name, list<expr>(), tmp);
@@ -940,7 +940,7 @@ struct structure_cmd_fn {
                 return elaborate_for_each<expr>(m_parents, tmp, std::bind(&structure_cmd_fn::elaborate_parent, this, false, _1, _2, _3, _4), [&](expr tmp) {
                     return elaborate_for_each<expr>(m_params, tmp, std::bind(&structure_cmd_fn::elaborate_local, this, true, _1, _2, _3, _4), [&](expr tmp) {
                         collect_implicit_locals(m_p, m_level_names, m_ctx_locals, tmp);
-                        return elaborate_for_each<expr>(m_ctx_locals, tmp, std::bind(&structure_cmd_fn::elaborate_local, this, true, _1, _2, _3, _4), [&](expr tmp) {
+                        return elaborate_for_each<expr>(m_ctx_locals, tmp, std::bind(&structure_cmd_fn::elaborate_local, this, false, _1, _2, _3, _4), [&](expr tmp) {
                             level_param_names new_ls;
                             expr new_tmp;
                             metavar_context mctx = m_ctx.mctx();
