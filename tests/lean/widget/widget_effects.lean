2047b436f394e73ae711fae4a88b9b9c3ef884a8
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2020-06-26 14:06:01 +0100

    test(widget): add a test for widget effects
    It still fails due to to_pos_info() not working?

---
+++ tests/lean/widget/widget_effects.lean
@@ -0,0 +1,63 @@
+open widget
+
+meta def effect_c : component tactic_state widget.effects :=
+widget.component.mk
+  unit
+  nat
+  (λ p s,(0) <| s)
+  (λ p ls ⟨⟩, ⟨ls + 1, some ([
+    effect.reveal_position "tests/lean/widget/widget_effects.lean" (pos.mk 60 14)
+  ])⟩)
+  (λ p s, [to_string s, button "+" (), " *** "]) (λ a b, ff)
+
+#html effect_c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- reveals __here!__
+
+
