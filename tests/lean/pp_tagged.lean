a1299209480072e1d54ecd16f9d209e7f9ea8cf7
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2020-06-30 23:33:33 +0100

    test(pp_tagged): draft test change
    Have a guess at what the correct behaviour of the test is.
    Will fix after CI has run.

--- tests/lean/pp_tagged.lean
+++ tests/lean/pp_tagged.lean
@@ -9,8 +9,6 @@ constants w x y z : nat
 
 #eval (sf.of_eformat <$> tactic.pp_tagged `(x = y)) >>= tactic.trace
 
-#eval (sf.of_eformat <$> tactic.pp_tagged `((int.of_nat = coe))) >>= tactic.trace
-
 #eval (sf.of_eformat <$> tactic.pp_tagged `((x,y))) >>= tactic.trace -- fa, a
 
 #eval (sf.of_eformat <$> tactic.pp_tagged `((w,x,y,z))) >>= tactic.trace -- fa, afa, aafa, aaa
@@ -18,5 +16,6 @@ constants w x y z : nat
 
 #eval (sf.of_eformat <$> tactic.pp_tagged `({w, x, y, z} : set nat)) >>= tactic.trace -- fa, afa, aafa, aaaa
 
+#eval (sf.of_eformat <$> tactic.pp_tagged `((int.of_nat = coe))) >>= tactic.trace
 constant bar [inhabited ℕ] [inhabited ℕ] : ℕ → ℤ
 #eval (sf.of_eformat <$> tactic.pp_tagged `((int.of_nat = bar))) >>= tactic.trace
