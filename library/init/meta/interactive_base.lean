ad5c5d1912dae2fd3a45542f5eef963d9d988fab
Mario Carneiro <di.gama@gmail.com>
2020-07-29 17:28:00 +0000

    fix(init/meta/interactive_base): description for small_nat (#424)
    Previously, any tactic using the `small_nat` parser without `with_desc` would get a messy error in the middle of the parser description. Now `small_nat` is described as `n` in parser patterns, e.g. `ident >> tk ":" *> small_nat?` becomes `id : n?`.

--- library/init/meta/interactive_base.lean
+++ library/init/meta/interactive_base.lean
@@ -104,6 +104,7 @@ private meta def parser_desc_aux : expr → tactic (list format)
 | `(ident)  := return ["id"]
 | `(ident_) := return ["id"]
 | `(parser.pexpr %%v) := return ["expr"]
+| `(small_nat) := return ["n"]
 | `(tk %%c) := list.ret <$> to_fmt <$> eval_expr string c
 | `(cur_pos) := return []
 | `(pure ._) := return []
