e760b0101430923d838b2d031d838dc0415fca68
Gabriel Ebner <gebner@gebner.org>
2020-07-30 09:59:27 +0200

    fix(frontends/lean/brackets): more set replacement fixes

--- tests/lean/brackets_parens.lean
+++ tests/lean/brackets_parens.lean
@@ -2,3 +2,14 @@ variables {α : Type} [has_one α]
 #check {(1 : α)}
 #check {(1), (2), (3)}
 #check ({(∘), (∘)} : set ((ℕ → ℕ) → (ℕ → ℕ) → (ℕ → ℕ)))
+
+#check {(0) + (0)}
+
+variables (a : α) [has_add α]
+#check {(a)}
+#check λ a, (by exact {(a)} : set α)
+#check {(a + b) | b ∈ (∅ : set α)}
+#check λ a, by exact {(a + b) | b ∈ (∅ : set α)}
+
+#check {(prod.mk 1 2).fst}
+#check {(prod.mk 1 b).fst | b ∈ ∅}
