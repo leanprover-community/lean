1c6527092088675632bd09fa159a2cfdb05f1691
Eric Wieser <wieser.eric@gmail.com>
2020-10-09 16:06:36 +0100

    fix(meta/converter/interactive): Cleanup and fix `find` too

--- library/init/meta/converter/interactive.lean
+++ library/init/meta/converter/interactive.lean
@@ -77,18 +77,32 @@ meta def find (p : parse parser.pexpr) (c : itactic) : conv unit :=
 do (r, lhs, _) ← tactic.target_lhs_rhs,
    pat ← tactic.pexpr_to_pattern p,
    s   ← simp_lemmas.mk_default, -- to be able to use congruence lemmas @[congr]
-   (found, new_lhs, pr) ←
-     tactic.ext_simplify_core ff {zeta := ff, beta := ff, single_pass := tt, eta := ff,
-                                  proj := ff, fail_if_unchanged := ff, memoize := ff} s
-       (λ u, return u)
-       (λ found s r p e, do
+   st ← tactic.read,
+   (found_result, new_lhs, pr) ← tactic.ext_simplify_core
+     (success ff st)  -- loop counter, and whether the conversion tactic failed
+     {zeta := ff, beta := ff, single_pass := tt, eta := ff, proj := ff,
+      fail_if_unchanged := ff, memoize := ff}
+     s
+     (λ u, return u)
+     (λ found_result s r p e,
+       -- if a previous conversion failed, then skip all subsequent conversions
+       match found_result with
+       | (success found s') := do
          guard (not found),
          matched ← (tactic.match_pattern pat e >> return tt) <|> return ff,
          guard matched,
-         ⟨new_e, pr⟩ ← c.convert e r,
-         return (tt, new_e, pr, ff))
-       (λ a s r p e, tactic.failed)
-       r lhs,
+         res ← capture (c.convert e r),
+         -- If an error occurs in conversion, capture it; `ext_simplify_core` will not
+         -- propagate it.
+         match res with
+         | (success r s')     := return (success tt s',    r.fst, some r.snd, ff)
+         | (exception f p s') := return (exception f p s', e,     none,       ff)
+         end
+       | (exception _ _ _) := tactic.failed
+       end)
+     (λ a s r p e, tactic.failed)
+     r lhs,
+  found ← unwrap found_result,
   when (not found) $ tactic.fail "find converter failed, pattern was not found",
   update_lhs new_lhs pr
 
@@ -97,40 +111,36 @@ do (r, lhs, _) ← tactic.target_lhs_rhs,
    pat ← tactic.pexpr_to_pattern p,
    s   ← simp_lemmas.mk_default, -- to be able to use congruence lemmas @[congr]
    st ← tactic.read,
-   (found, new_lhs, pr) ← tactic.ext_simplify_core
+   (found_result, new_lhs, pr) ← tactic.ext_simplify_core
      (success 1 st)  -- loop counter, and whether the conversion tactic failed
      {zeta := ff, beta := ff, single_pass := tt, eta := ff, proj := ff,
       fail_if_unchanged := ff, memoize := ff}
      s
      (λ u, return u)
-     (λ i s r p e,
+     (λ found_result s r p e,
        -- if a previous conversion failed, then skip all subsequent conversions
-       match i with
-       | (success n s') :=
+       match found_result with
+       | (success i s') :=
          do matched ← (tactic.match_pattern pat e >> return tt) <|> return ff,
             guard matched,
-            if n ∈ occs then 
-              (λ s,
-                -- If an error occurs in conversion, capture it; `ext_simplify_core` will not
-                -- propagate it.
-                match (c.convert e r) s with
-                | (success r s')     := success (success (n+1) s', r.fst, some r.snd, tt) s'
-                | (exception f p s') := success (exception f p s', e,     none,       tt) s'
-                end
-              )
+            if i ∈ occs then do
+              res ← capture (c.convert e r),
+              -- If an error occurs in conversion, capture it; `ext_simplify_core` will not
+              -- propagate it.
+              match res with
+              | (success r s')     := return (success (i+1) s', r.fst, some r.snd, tt)
+              | (exception f p s') := return (exception f p s', e,     none,       tt)
+              end
             else do
               st ← tactic.read,
-              return (success (n+1) st, e, none, tt)
+              return (success (i+1) st, e, none, tt)
        | (exception _ _ _) := tactic.failed
        end)
      (λ a s r p e, tactic.failed)
      r lhs,
   -- Re-throw any error captured inside `ext_simplify_core`
-  (λ s, match found with
-  | (success r s') :=  (success r s)
-  | (exception f p s') := (exception f p s')
-  end),
-  conv.update_lhs new_lhs pr
+  unwrap found_result,
+  update_lhs new_lhs pr
 
 meta def simp (no_dflt : parse only_flag) (hs : parse tactic.simp_arg_list) (attr_names : parse with_ident_list)
               (cfg : tactic.simp_config_ext := {})
