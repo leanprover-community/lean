3e0b4d90305f677b4ce54e8c9095746e12458fab
Eric Wieser <wieser.eric@gmail.com>
2020-10-12 20:53:52 +0100

    Update tests

---
+++ tests/lean/conv_for_find.lean
@@ -0,0 +1,48 @@
+-- typos should be caught
+example {a b c : ℕ} : (a * b) * c = (a * b) * c := begin
+  conv {
+    rw does_not_exist,
+  },
+end
+
+example {a b c : ℕ} : (a * b) * c = (a * b) * c := begin
+  conv {
+    for (_ * _) [1] {
+      rw does_not_exist,
+    },
+  },
+end
+
+example {a b c : ℕ} : (a * b) * c = (a * b) * c := begin
+  conv {
+    find (_ * _) {
+      rw does_not_exist,
+    },
+  },
+end
+
+-- filled in metavariables should remain filled
+example {a b : ℕ} : ∃ x, x * a = a * x :=  begin
+  existsi _,
+  conv {
+    rw nat.mul_comm b a,
+  },
+end
+
+example {a b : ℕ} : ∃ x, x * a = a * x :=  begin
+  existsi _,
+  conv {
+    find (_ * _) {
+      rw nat.mul_comm b a,
+    },
+  },
+end
+
+example {a b : ℕ} : ∃ x, x * a = a * x :=  begin
+  existsi _,
+  conv {
+    for (_ * _) [1] {
+      rw nat.mul_comm b a,
+    },
+  },
+end
--- tests/lean/fail/conv_for.lean
+++
@@ -1,7 +0,0 @@
-example {a b c : ℕ} : (a * b) * c = (a * b) * c := begin
-  conv {
-    for (_ * _) [1] {
-      rw does_not_exist,
-    },
-  },
-end
