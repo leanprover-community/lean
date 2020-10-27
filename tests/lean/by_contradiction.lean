1d0b64b358c470fb600782cca7844cda8185d1ef
Mario Carneiro <di.gama@gmail.com>
2020-10-27 14:34:58 -0400

    feat(init/meta/tactic): by_contradiction tweaks
    This adds support for classical `by_contradiction`, as well as always using
    `intro` instead of `intro1` (which picks up the binder variable of `not`,
    which is never meaningful), instead using `h` as default name.

--- tests/lean/by_contradiction.lean
+++ tests/lean/by_contradiction.lean
@@ -12,23 +12,13 @@ by do
 example (a b : nat) : ¬¬ a = b → a = b :=
 by do
   intros,
-  by_contradiction `H,
+  by_contradiction,
   trace_state,
   contradiction
 
 #print "-------"
 
 example (p q : Prop) : ¬¬ p → p :=
-by do
-  intros,
-  by_contradiction `H, -- should fail
-  trace_state
-
-#print "-------"
-
-local attribute [instance] classical.prop_decidable -- Now all propositions are decidable
-
-example (p q : Prop) : ¬¬p → p :=
 by do
   intros,
   by_contradiction `H,
