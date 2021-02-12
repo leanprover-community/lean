69921bdd2f373aabcf8b22eb7472f8ef47819e17
Daniel Selsam <daniel.selsam@protonmail.com>
2021-02-12 10:52:09 -0800

    chore: revert test to use 'example' instead of 'def'

--- tests/lean/eformat.lean
+++ tests/lean/eformat.lean
@@ -11,31 +11,31 @@ meta def trace_tagged_debug_locals : tactic unit := do
   cs ← cs.mmap infer_type,
   cs.mmap' trace_tagged_debug
 
-def example1 (α : Type) (x : α) (h : 1 = 2) (f : ite ff true false) (g : x ≠ x) : true :=
+example (α : Type) (x : α) (h : 1 = 2) (f : ite ff true false) (g : x ≠ x) : true :=
 begin
   trace_tagged_debug_locals,
   trivial
 end
 
-def example2 (k : (-3 : int) < (1 + 1 : nat)) : true :=
+example (k : (-3 : int) < (1 + 1 : nat)) : true :=
 begin
   trace_tagged_debug_locals, -- note the coe is present.
   trivial
 end
 
-def example3 (k : Π {α β : Type}, α ≠ β) : true :=
+example (k : Π {α β : Type}, α ≠ β) : true :=
 begin
   trace_tagged_debug_locals,
   trivial
 end
 
-def example4 (k : (λ α β : Type, α ≠ β) Prop Prop) : true :=
+example (k : (λ α β : Type, α ≠ β) Prop Prop) : true :=
 begin
   trace_tagged_debug_locals,
   trivial
 end
 
-def example5 (S : set nat) (k : S = {3, 2, 1}) : true :=
+example (S : set nat) (k : S = {3, 2, 1}) : true :=
 begin
   trace_tagged_debug_locals,
   trivial
