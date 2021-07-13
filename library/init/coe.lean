12c9123740335d6986afbe850ad3f5c785fdd78b
Nicolò Cavalleri <nico@cavalleri.net>
2021-07-13 12:28:42 +0200

    Adding backticks

--- library/init/coe.lean
+++ library/init/coe.lean
@@ -11,21 +11,21 @@ import init.data.list.basic init.data.subtype.basic init.data.prod
 /-! # Coercions and lifts
 
 The elaborator tries to insert coercions automatically.
-Only instances of has_coe type class are considered in the process.
+Only instances of `has_coe` type class are considered in the process.
 
-Lean also provides a "lifting" operator: ↑a.
-It uses all instances of has_lift type class.
-Every has_coe instance is also a has_lift instance.
+Lean also provides a "lifting" operator: `↑a`.
+It uses all instances of `has_lift` type class.
+Every `has_coe` instance is also a `has_lift` instance.
 
-We recommend users only use has_coe for coercions that do not produce a lot
+We recommend users only use `has_coe` for coercions that do not produce a lot
 of ambiguity.
 
-All coercions and lifts can be identified with the constant coe.
+All coercions and lifts can be identified with the constant `coe`.
 
-We use the has_coe_to_fun type class for encoding coercions from
+We use the `has_coe_to_fun` type class for encoding coercions from
 a type to a function space.
 
-We use the has_coe_to_sort type class for encoding coercions from
+We use the `has_coe_to_sort` type class for encoding coercions from
 a type to a sort.
 -/
 
@@ -35,14 +35,14 @@ universes u v
 class has_lift (a : Sort u) (b : Sort v) :=
 (lift : a → b)
 
-/-- Auxiliary class that contains the transitive closure of has_lift. -/
+/-- Auxiliary class that contains the transitive closure of `has_lift`. -/
 class has_lift_t (a : Sort u) (b : Sort v) :=
 (lift : a → b)
 
 class has_coe (a : Sort u) (b : Sort v) :=
 (coe : a → b)
 
-/-- Auxiliary class that contains the transitive closure of has_coe. -/
+/-- Auxiliary class that contains the transitive closure of `has_coe`. -/
 class has_coe_t (a : Sort u) (b : Sort v) :=
 (coe : a → b)
 
@@ -102,26 +102,26 @@ instance coe_trans {a : Sort u₁} {b : Sort u₂} {c : Sort u₃} [has_coe_t b 
 instance coe_base {a : Sort u} {b : Sort v} [has_coe a b] : has_coe_t a b :=
 ⟨coe_b⟩
 
-/-- We add this instance directly into has_coe_t to avoid non-termination.
+/-- We add this instance directly into `has_coe_t` to avoid non-termination.
 
-   Suppose coe_option had type (has_coe a (option a)).
-   Then, we can loop when searching a coercion from α to β (has_coe_t α β)
-   1- coe_trans at (has_coe_t α β)
-          (has_coe α ?b₁) and (has_coe_t ?b₁ c)
-   2- coe_option at (has_coe α ?b₁)
-          ?b₁ := option α
-   3- coe_trans at (has_coe_t (option α) β)
-          (has_coe (option α) ?b₂) and (has_coe_t ?b₂ β)
-   4- coe_option at (has_coe (option α) ?b₂)
-          ?b₂ := option (option α))
+   Suppose coe_option had type `(has_coe a (option a))`.
+   Then, we can loop when searching a coercion from `α` to `β` `(has_coe_t α β)`
+   1- `coe_trans at (has_coe_t α β)`
+          `(has_coe α ?b₁) and (has_coe_t ?b₁ c)`
+   2- `coe_option at (has_coe α ?b₁)`
+          `?b₁ := option α`
+   3- `coe_trans at (has_coe_t (option α) β)`
+          `(has_coe (option α) ?b₂) and (has_coe_t ?b₂ β)`
+   4- `coe_option at (has_coe (option α) ?b₂)`
+          `?b₂ := option (option α))`
    ... -/
 instance coe_option {a : Type u} : has_coe_t a (option a) :=
 ⟨λ x, some x⟩
 
-/-- Auxiliary transitive closure for has_coe which does not contain
-   instances such as coe_option.
+/-- Auxiliary transitive closure for `has_coe` which does not contain
+   instances such as `coe_option`.
 
-   They would produce non-termination when combined with coe_fn_trans and coe_sort_trans. -/
+   They would produce non-termination when combined with `coe_fn_trans` and `coe_sort_trans`. -/
 class has_coe_t_aux (a : Sort u) (b : Sort v) :=
 (coe : a → b)
 
@@ -151,7 +151,7 @@ instance coe_bool_to_Prop : has_coe bool Prop :=
 /-- Tactics such as the simplifier only unfold reducible constants when checking whether two terms are definitionally
    equal or a term is a proposition. The motivation is performance.
    In particular, when simplifying `p -> q`, the tactic `simp` only visits `p` if it can establish that it is a proposition.
-   Thus, we mark the following instance as @[reducible], otherwise `simp` will not visit `↑p` when simplifying `↑p -> q`. -/
+   Thus, we mark the following instance as `@[reducible]`, otherwise `simp` will not visit `↑p` when simplifying `↑p -> q`. -/
 @[reducible] instance coe_sort_bool : has_coe_to_sort bool :=
 ⟨Prop, λ y, y = tt⟩
 
@@ -165,7 +165,7 @@ instance coe_subtype {a : Sort u} {p : a → Prop} : has_coe {x // p x} a :=
 
 universes ua ua₁ ua₂ ub ub₁ ub₂
 
-/-- Remark: we can't use [has_lift_t a₂ a₁] since it will produce non-termination whenever a type class resolution
+/-- Remark: we can't use `[has_lift_t a₂ a₁]` since it will produce non-termination whenever a type class resolution
    problem does not have a solution. -/
 instance lift_fn {a₁ : Sort ua₁} {a₂ : Sort ua₂} {b₁ : Sort ub₁} {b₂ : Sort ub₂} [has_lift a₂ a₁] [has_lift_t b₁ b₂] : has_lift (a₁ → b₁) (a₂ → b₂) :=
 ⟨λ f x, ↑(f ↑x)⟩
