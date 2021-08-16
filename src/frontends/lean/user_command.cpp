fc5f1191859fe1074c5fb00aabace91b16fa2cf2
Mario Carneiro <di.gama@gmail.com>
2021-08-16 15:10:19 -0400

    fix(user_command): get AST for command parser

--- src/frontends/lean/user_command.cpp
+++ src/frontends/lean/user_command.cpp
@@ -86,7 +86,7 @@ static environment add_user_command(environment const & env, name const & d) {
             args.push_back(to_obj(meta));
         }
         parser = p.elaborate("_user_command", {}, parser).first;
-        run_parser(p, parser, args, true);
+        data.push(run_parser(p, parser, args, true).first);
         return p.env();
     };
 
