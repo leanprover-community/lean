73d5d20dcee8ad93b346f12bbb0e806e6f78abdb
Scott Morrison <scott@tqft.net>
2021-08-20 13:11:38 +1000

    oops

--- tests/lean/run/empty_match.lean
+++ tests/lean/run/empty_match.lean
@@ -1,8 +1,8 @@
 open nat
 
-definition not_false (a : nat) : ¬ false :=
+definition not_false' (a : nat) : ¬ false :=
 assume H : false,
 match H with
 end
 
-#check _root_.not_false
+#check _root_.not_false'
