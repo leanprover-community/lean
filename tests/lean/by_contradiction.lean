387a776fd1a04336b124a8e58d0e7e32ed642998
Eric Wieser <wieser.eric@gmail.com>
2021-09-21 17:30:37 +0100

    Update by_contradiction.lean

--- tests/lean/by_contradiction.lean
+++ tests/lean/by_contradiction.lean
@@ -24,3 +24,26 @@ by do
   by_contradiction `H,
   trace_state,
   contradiction
+
+-- following tests use interactive mode so that we can use `guard_hyp`
+
+
+/-- `by_contradiction` should unfold reducible defs like `ne` to find a leading `not`. --/
+example (a b : nat) : a ≠ b → a ≠ b :=
+begin
+  intros,
+  by_contradiction H,
+  guard_hyp H : a = b,
+  contradiction
+end
+
+def my_ne (a b : nat) := a ≠ b
+
+/-- Semireducible defs are not unfolded -/
+example (a b : nat) : a ≠ b → my_ne a b :=
+begin
+  intros,
+  by_contradiction H,
+  guard_hyp H : ¬my_ne a b,
+  contradiction
+end
