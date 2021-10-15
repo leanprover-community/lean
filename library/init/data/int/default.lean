a9804247b0c3361c2b1b601daa5f589b5bcfe6f3
YaelDillies <yael.dillies@gmail.com>
2021-10-15 12:43:32 +0100

    initial commit

--- library/init/data/int/default.lean
+++
@@ -1,8 +0,0 @@
-/-
-Copyright (c) 2016 Microsoft Corporation. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura
--/
-prelude
-import init.data.int.basic init.data.int.order init.data.int.comp_lemmas
-import init.data.int.bitwise
