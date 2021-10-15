812d5cfa6e4f469295bd6140e9f0b244636919a6
Scott Morrison <scott.morrison@gmail.com>
2021-10-15 20:24:43 +1100

    fix test

--- tests/lean/run/order_defaults.lean
+++ tests/lean/run/order_defaults.lean
@@ -1,21 +1,17 @@
-example : preorder unit := {
-    le := λ _ _, true,
-    le_refl := λ _, trivial,
-    le_trans := λ _ _ _ _ _, trivial,
-}
+example : preorder unit :=
+{ le := λ _ _, true,
+  le_refl := λ _, trivial,
+  le_trans := λ _ _ _ _ _, trivial, }
 
-example : partial_order unit := {
-    le := λ _ _, true,
-    le_refl := λ _, trivial,
-    le_trans := λ _ _ _ _ _, trivial,
-    le_antisymm := by intros a b; intros; cases a; cases b; refl
-}
+example : partial_order unit :=
+{ le := λ _ _, true,
+  le_refl := λ _, trivial,
+  le_trans := λ _ _ _ _ _, trivial,
+  le_antisymm := by intros a b; intros; cases a; cases b; refl }
 
-example : linear_order unit := {
-    le := λ _ _, true,
-    le_refl := λ _, trivial,
-    le_trans := λ _ _ _ _ _, trivial,
-    le_antisymm := by intros a b; intros; cases a; cases b; refl,
-    le_total := λ _ _, or.inl trivial,
-    decidable_le := infer_instance
-}
+example : linear_order unit :=
+{ le := λ _ _, true,
+  le_refl := λ _, trivial,
+  le_trans := λ _ _ _ _ _, trivial,
+  le_antisymm := by intros a b; intros; cases a; cases b; refl,
+  le_total := λ _ _, or.inl trivial }
