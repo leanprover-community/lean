25fbaf4d89384dd9778721de42a28487e7c19e8b
Kyle Miller <kmill31415@gmail.com>
2022-02-22 10:34:46 -0800

    added extra tests

--- tests/lean/sorry_itactic.lean
+++ tests/lean/sorry_itactic.lean
@@ -11,3 +11,19 @@ begin
   sorry,
   { intro h, assumption, },
 end
+
+-- Make sure other ways to comment out blocks still work:
+
+example (p : Prop) : p ↔ p :=
+begin
+  split,
+  sorry; { intro h, assumption, },
+  { intro h, assumption, },
+end
+
+example (p : Prop) : p ↔ p :=
+begin
+  split,
+  sorry <|> { intro h, assumption, },
+  { intro h, assumption, },
+end
