4e04699a77fc9a0b5790c66ddef9016fd601ae30
Kyle Miller <kmill31415@gmail.com>
2022-03-08 09:13:11 +0000

    feat(src/library/noncomputable): for constructors, only check arguments after inductive type's parameters (#693)
    This adds functionality to the noncomputability checker to skip arguments to a constructor that correspond to parameters for its corresponding inductive type. These have no computational relevance. This brings it more in line with `src/library/compiler/simp_inductive`, which only looks at computationally relevant arguments after the parameters. This also appears to be closer to Lean 4's behavior.

--- src/library/noncomputable.cpp
+++ src/library/noncomputable.cpp
@@ -16,6 +16,7 @@ Author: Leonardo de Moura
 #include "library/trace.h"
 #include "library/quote.h"
 #include "library/constants.h"
+#include "library/vm/vm.h"
 // TODO(Leo): move inline attribute declaration to library
 #include "library/compiler/inliner.h"
 namespace lean {
@@ -167,8 +168,15 @@ struct get_noncomputable_reason_fn {
                 }
             }
             visit(fn);
-            for (expr const & arg : args)
-                visit(arg);
+            unsigned start = 0;
+            if (is_constant(fn) && !is_vm_builtin_function(const_name(fn))) {
+                if (auto I_name = inductive::is_intro_rule(m_tc.env(), const_name(fn))) {
+                    // For constructors, only visit arguments after the inductive type's parameters
+                    start = *inductive::get_num_params(m_tc.env(), *I_name);
+                }
+            }
+            for (unsigned i = start; i < args.size(); i++)
+                visit(args[i]);
         }
     }
 
