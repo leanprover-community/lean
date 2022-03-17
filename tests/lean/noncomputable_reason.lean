dd262a22adb32a3a4e36cbf7465e36ba8b7573dd
Kyle Miller <kmill31415@gmail.com>
2022-03-17 16:59:00 -0700

    additional test

--- tests/lean/noncomputable_reason.lean
+++ tests/lean/noncomputable_reason.lean
@@ -2,6 +2,8 @@ constant foo : ℕ
 
 noncomputable def bar : ℕ := foo
 
+noncomputable lemma bar' : ℕ := 37
+
 def baz : ℕ := 0
 
 open tactic
@@ -9,4 +11,5 @@ run_cmd do
   e ← get_env,
   trace $ e.decl_noncomputable_reason `foo,
   trace $ e.decl_noncomputable_reason `bar,
+  trace $ e.decl_noncomputable_reason `bar',
   trace $ e.decl_noncomputable_reason `baz
