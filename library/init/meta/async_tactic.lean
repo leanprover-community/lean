06cf2fad4a9a72d375c8077e47ae7ab8203f6744
Johan Commelin <johan@commelin.net>
2022-03-23 10:15:45 +0000

    fix(library/init/meta/async_tactic): make async aware of instance cache
    Kudos to @gebner for the pointer!

--- library/init/meta/async_tactic.lean
+++ library/init/meta/async_tactic.lean
@@ -28,7 +28,7 @@ s ← read, return $ task.delay $ λ _,
   end
 
 meta def prove_goal_async (tac : tactic unit) : tactic unit := do
-ctx ← local_context, revert_lst ctx,
+ctx ← local_context, unfreezing (revert_lst ctx),
 tgt ← target, tgt ← instantiate_mvars tgt,
 env ← get_env, tgt ← return $ env.unfold_untrusted_macros tgt,
 when tgt.has_meta_var (fail "goal contains metavariables"),
@@ -37,7 +37,7 @@ lemma_name ← new_aux_decl_name,
 proof ← run_async (do
   goal_meta ← mk_meta_var tgt,
   set_goals [goal_meta],
-  ctx.mmap' (λc, intro c.local_pp_name),
+  ctx.mmap' (λc, unfreezing (intro c.local_pp_name)),
   tac,
   proof ← instantiate_mvars goal_meta,
   proof ← return $ env.unfold_untrusted_macros proof,
