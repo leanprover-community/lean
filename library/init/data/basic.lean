81bbf9dab726ed802c99a1bde29c464b1f997c79
YaelDillies <yael.dillies@gmail.com>
2022-06-07 22:41:16 +0100

    remove init.data.set from init.data.basic

--- library/init/data/basic.lean
+++ library/init/data/basic.lean
@@ -8,6 +8,6 @@ import init.data.setoid init.data.quot init.data.bool.basic init.data.punit
 import init.data.nat.basic init.data.prod init.data.sum.basic
 import init.data.sigma.basic init.data.subtype.basic
 import init.data.fin.basic init.data.list.basic init.data.char.basic
-import init.data.string.basic init.data.option.basic init.data.set
+import init.data.string.basic init.data.option.basic
 import init.data.unsigned.basic init.data.ordering.basic init.data.repr
 import init.data.to_string
