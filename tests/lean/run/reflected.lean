4ae84bfb3e271f8889c7a677ed6418bf0a8a8489
Eric Wieser <wieser.eric@gmail.com>
2022-06-23 17:05:20 +0000

    move tests

--- tests/lean/run/reflected.lean
+++ tests/lean/run/reflected.lean
@@ -2,14 +2,3 @@ meta def eval_list (α : Type) [reflected _ α] (e : expr) : tactic (list α) :=
 tactic.eval_expr (list α) e
 
 run_cmd eval_list ℕ ↑`([1, 2]) >>= tactic.trace
-
-def synonym (α : Type) : Type := α
-
-def synonym.of {α : Type} (a : α) : synonym α := a
-
-meta instance synonym.reflect {α : Type} [h : has_reflect α] [reflected _ α] :
-  has_reflect (synonym α) :=
-λ x, show reflected _ (synonym.of x), from `(synonym.of).subst (h x)
-
-run_cmd let x := synonym.of 1 in tactic.trace `(x)
-run_cmd let x := 1 in tactic.trace `(x)
