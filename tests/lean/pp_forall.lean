e483cf620be08a98d3edba14665dfebce4b77cb3
Kyle Miller <kmill31415@gmail.com>
2022-11-15 14:09:37 +0100

    extra tests for multiple binders

--- tests/lean/pp_forall.lean
+++ tests/lean/pp_forall.lean
@@ -11,3 +11,14 @@ variables (p q : Prop)
 #check Π {n : ℕ}, ℤ
 #check Π (n : ℕ), fin n
 #check ∀ n : ℕ, ¬∃ x : ℕ, x ≠ x
+#check Π (m n : ℕ), ℤ
+#check Π (m n : ℕ), fin m
+#check Π (m n : ℕ), fin n
+#check ∀ (m n : ℕ), 1 = 2
+#check ∀ (m n : ℕ), m = 1
+#check ∀ (m n : ℕ), n = 1
+#check ∀ (m n : ℕ), m = n
+#check ∀ (m : ℕ) (h : p), q ∧ m = 1
+#check ∀ (m : ℕ) (h : p), q
+#check ∀ (h : p) (m : ℕ), q ∧ m = 1
+#check ∀ (h : p) (m : ℕ), q
