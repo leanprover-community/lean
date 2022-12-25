9d3366ba27b9ec4469eaef019fc43fb513838d00
Gabriel Ebner <gebner@gebner.org>
2022-12-25 13:27:59 -0800

    pacify linter

--- src/library/tlean_exporter.cpp
+++ src/library/tlean_exporter.cpp
@@ -5,6 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura, Daniel Selsam
 */
 #include <iostream>
+#include <string>
 #include "library/tlean_exporter.h"
 #include "library/unfold_macros.h"
 #include "kernel/quotient/quotient.h"
