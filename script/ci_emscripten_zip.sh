cf6f971e5a0ddf90a807de941db531d506739220
Eric Wieser <wieser.eric@gmail.com>
2023-05-02 01:45:49 +0100

    Update the emscripten container
    Also adjusts build rules to link the now not-include-by-default `NODEFS` library.

--- script/ci_emscripten_zip.sh
+++ script/ci_emscripten_zip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -Eeo pipefail
 # can't run build/shell/lean directly since the path in it is set by the docker container
 LEAN_VERSION_DIGITS=$(node build/shell/lean.js -v | sed 's/Lean (version \([^,]*\),.*/\1/')
 zip build/lean-$LEAN_VERSION_DIGITS-$LEAN_VERSION_STRING-browser.zip build/shell/lean_js_*
