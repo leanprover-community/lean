21d264a66d53b0a910178ae7d9529cb5886a39b6
Jonathan Protzenko <protz@microsoft.com>
2023-09-27 18:22:08 +0000

    fix(shell): add missing include (#813)
    This header inclusion was missing, resulting in build errors with recent versions of GCC:
    
    ```
    In file included from /home/jonathan/Code/lean3/src/shell/lean_js_main.cpp:9:
    /home/jonathan/Code/lean3/src/shell/lean_js.h:11:32: error: ‘uintptr_t’ was not declared in this scope
       11 | int emscripten_process_request(uintptr_t msg);
    ```

--- src/shell/lean_js.h
+++ src/shell/lean_js.h
@@ -5,6 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #pragma once
+#include <cstdint>
 #include <string>
 void initialize_emscripten();
 void finalize_emscripten();
